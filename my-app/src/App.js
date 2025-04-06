f7b8cbcf0f7853a0d8907b0b312a76c27c79e479
Khushi Agrawal <khushiagrawal@Khushis-MacBook-Air-3.local>
2025-04-05 20:39:52 -0500

    resolved conflicts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
-import './App.css';
-import LoginRegister from './components/LoginRegister';
-
-function App() {
-  return (
-    <div className="App">
-      <LoginRegister />
-    </div>
-  );
-=======
 import HomePage from './components/HomePage';
 
 function App() {
   return <HomePage />;
->>>>>>> 41929539bcc0963a17a776a39e8141ad6e653b20
 }
 
 export default App;
